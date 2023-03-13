86a4236018e610351f6b94d852acef946421943c
Jorge Martinez <george16180@live.com>
2023-03-13 13:59:38 -0500

    first commit

---
+++ src/main/java/com/breeds/DogsandPokemons/DogsAndPokemonsApplication.java
@@ -0,0 +1,13 @@
+package com.breeds.DogsandPokemons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsAndPokemonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsAndPokemonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breeds/DogsandPokemons/DogsAndPokemonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breeds.DogsandPokemons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogsAndPokemonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
